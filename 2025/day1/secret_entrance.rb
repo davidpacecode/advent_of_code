9725a30e5344efb7edfd27edf55dbe67aa8de6e7
davidpacecode <davidpace@gmail.com>
2025-12-13 15:16:12 -0600

    would work if the turns were < 100. just realized there are bigger number of turns. About to fix that but want to keep this version, too.

--- 2025/day1/secret_entrance.rb
+++ 2025/day1/secret_entrance.rb
@@ -1,42 +1,30 @@
 #!/home/dsp2/.local/share/mise/installs/ruby/3.3.4/bin/ruby
 
 dial = Array.new(100) { |i| i }
-
-def adjust_dial (current, direction, amount)
-  puts "current before the adjustment = #{current}"
-  
-  # direction == "L" ? current = current - amount : current = current + amount
-  if direction == "L"
-    current-= amount
-  else
-    current+= amount
-  end
-
-  puts "current after the adjustment = #{current}"
-
-  direction == "L" ? "direction is L" : "direction is R"
-  current < 0 ? "current is less than 0" : "current is more than 0"
-  if current < 0
-    puts "current is less than 0"
-    current = 100 - current
-  elsif current >= 100
-    puts "current is greater than or equal to 100"
-    current = current - 100
-  else
-    puts "current is good"
-    current
-  end
-end
-
-
-
 zero_count = 0
 index = 50
+zero_count = 0
 
 File.read("input.txt").split("\n").each do |line|
-# line[0] == "L" ? index-=line[1..2].to_i : index+=line[1..2].to_i
-  puts "line = #{line} index = #{index} direction = #{line[0]} amount = #{line[1..2].to_i}"
-  index = adjust_dial(index, line[0], line[1..2].to_i)
-  puts "#{index}"
+
+  if line[0] == 'L'
+    if (index - line[1..2].to_i) < 0
+      index = 100 - (line[1..2].to_i - index)
+    else
+      index-= line[1..2].to_i
+    end
+  else
+    if (index + line[1..2].to_i) >= 100
+      index = index + line[1..2].to_i - 100
+    else
+      index+= line[1..2].to_i
+    end
+  end
+
+  zero_count+= 1 if index == 0
+
+  puts "- The dial is rotated #{line} to point at #{index}"
 end
+
+puts "the secret code is #{zero_count}"
 
