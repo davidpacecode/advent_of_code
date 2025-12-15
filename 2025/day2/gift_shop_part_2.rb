c326c5e8ffecf87059100f21ac8457987a16d384
davidpacecode <davidpace@gmail.com>
2025-12-14 19:03:34 -0600

    I think i have the frame and check for repeating things working in day 2 part 2. now i need to iterate through things, call these suckers, and sum it all up and then......hope.....

---
+++ 2025/day2/gift_shop_part_2.rb
@@ -0,0 +1,42 @@
+#!/home/dsp2/.local/share/mise/installs/ruby/3.3.4/bin/ruby
+
+def sum_codes(sub_str, num)
+  5
+end
+
+def is_repeating(sub_str, num)
+  comp_str = ""
+  num_iterations = num.to_s.length / sub_str.to_s.length
+  num_iterations.times do
+    comp_str = comp_str.concat(sub_str.to_s)
+  end
+  puts "comp_str is #{comp_str}"
+  puts "num is #{num}"
+  comp_str == num.to_s ? true : false
+end
+
+sum = 0
+
+File.read("input_part_2.txt").split(",").each do |line|
+
+  puts "#{line.split('-')[0].to_i} - #{line.split('-')[1].to_i}"
+  (line.split('-')[0].to_i..line.split('-')[1].to_i).each do |num|
+    if num.even?
+      sum += sum_codes(num,num/2)
+    else
+      sum += sum_codes(num,num/3)
+    end
+  end
+end
+
+
+puts "11 11 #{is_repeating(11, 11)}"
+puts "11 111111 #{is_repeating(11, 111111)}"
+puts "1024 10241023 #{is_repeating(1024, 10241023)}"
+# puts "11 11 #{is_repeating(11, 11)}"
+# puts "11 11 #{is_repeating(11, 11)}"
+# puts "11 11 #{is_repeating(11, 11)}"
+# puts "11 11 #{is_repeating(11, 11)}"
+# puts "11 11 #{is_repeating(11, 11)}"
+# puts "11 11 #{is_repeating(11, 11)}"
+
