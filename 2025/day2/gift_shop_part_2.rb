ee330f66cd0a37ca96af08d2436a8275bb1e5e3b
davidpacecode <davidpace@gmail.com>
2025-12-15 19:11:45 -0600

    i think i'm getting closer to day 2 part 2. still not working, tho

--- 2025/day2/gift_shop_part_2.rb
+++ 2025/day2/gift_shop_part_2.rb
@@ -2,39 +2,46 @@
 
 def sum_codes(sub_str, num)
   sum = 0
-  sub_str.to_s.length.downto(1) do |i|
-    puts "i is #{i}"
+  sub_str.length - 1.downto(1) do |i|
+    if num.to_i % sub_str.to_i == 0
+      if is_repeating(sub_str[..i], num)
+        sum += num.to_i
+        print "#{num} "
+      end
+    end
   end
- 5 
+  sum
 end
 
 def is_repeating(sub_str, num)
   comp_str = ""
-  num_iterations = num.to_s.length / sub_str.to_s.length
+  num_iterations = num.length / sub_str.length
   num_iterations.times do
-    comp_str = comp_str.concat(sub_str.to_s)
+    comp_str = comp_str.concat(sub_str)
   end
-  comp_str == num.to_s ? true : false
+  comp_str == num ? true : false
 end
 
 sum = 0
 
 File.read("input_part_2.txt").split(",").each do |line|
 
-  puts "#{line.split('-')[0].to_i} - #{line.split('-')[1].to_i}"
+  print "#{line.split('-')[0].to_i} - #{line.split('-')[1].to_i}: "
   (line.split('-')[0].to_i..line.split('-')[1].to_i).each do |num|
     if num.even?
-      sum += sum_codes(num,num/2)
+      sum += sum_codes(num.to_s[0..(num.to_s.length/2 - 1)],num.to_s)
     else
-      sum += sum_codes(num,num/3)
+      sum += sum_codes(num.to_s[0..(num.to_s.length/3 - 1)],num.to_s)
     end
   end
+  puts
 end
 
+puts "sum: #{sum}"
 
-puts "11 11 #{is_repeating(11, 11)}"
-puts "11 111111 #{is_repeating(11, 111111)}"
-puts "1024 10241023 #{is_repeating(1024, 10241023)}"
+# puts "11 11 #{is_repeating("11", "11")}"
+# puts "11 111111 #{is_repeating("11", "111111")}"
+# puts "1024 10241023 #{is_repeating("1024", "10241023")}"
 # puts "11 11 #{is_repeating(11, 11)}"
 # puts "11 11 #{is_repeating(11, 11)}"
 # puts "11 11 #{is_repeating(11, 11)}"
