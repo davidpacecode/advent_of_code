98dc469a0c81e856aefec84db0f6ef24c986b6e3
David Pace <david.pace@hey.com>
2025-12-17 13:54:15 -0600

    working on day 3 part 1...it's almost working i think

---
+++ 2025/day3/lobby.rb
@@ -0,0 +1,30 @@
+#!/home/dsp2/.local/share/mise/installs/ruby/3.4.7/bin/ruby
+
+sum = 0
+
+File.open("input.txt").each_line do |line|
+
+  arr = line.chomp.chars.map(&:to_i)
+  rev_arr = arr.reverse
+  tens = arr.max
+  tens_index = arr.size - rev_arr.index(tens) - 1
+
+  # puts "#{line}"
+  # puts "tens_index is #{tens_index} and arr.size is #{arr.size}"
+  # arr.each.with_index do |a, index|
+  #   puts "arr[#{index}] is #{a}"
+  # end
+
+  if tens_index == arr.size - 1
+    ones = tens
+    tens = arr[0..tens_index - 1].max
+  else
+    ones = arr[tens_index + 1 .. arr.size - 1].max
+  end
+
+  sum += tens * 10 + ones
+  print "#{line.chomp} -> "
+  puts "#{tens * 10 + ones}"
+end
+
+puts "sum: #{sum}"
