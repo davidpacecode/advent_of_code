fa721bdfcd60e619347e7175144b8820361b98d3
David Pace <david.pace@hey.com>
2025-12-18 13:39:30 -0600

    started day 3 part 2...i think i have the bottom 3 set working or at least started

--- 2025/day3/lobby_part_2.rb
+++ 2025/day3/lobby_part_2.rb
@@ -1,11 +1,21 @@
 #!/home/dsp2/.local/share/mise/installs/ruby/3.4.7/bin/ruby
 
+require 'set'
 
 def find_bottom_3(arr)
 
   count = 0
+  bottom_3 = Set.new
 
-
+  (1..9).each do |i|
+    arr.count(i).times do
+      bottom_3.add(i) unless count > 3
+      count += 1
+      break if count >= 3
+    end
+    break if count >= 3
+  end
+  bottom_3
 end
 
 sum = 0
@@ -14,6 +24,9 @@ File.open("input.txt").each_line do |line|
 
   arr = line.chomp.chars.map(&:to_i)
 
+  bottom_3 = find_bottom_3(arr)
+  puts "the bottom_3 of #{line} are #{bottom_3.to_a.to_s}"
 end
 
 puts "sum: #{sum}"
+
