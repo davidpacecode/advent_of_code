4250ee8da8a7d9287ceb72b58c964f060bda2125
David Pace <david.pace@hey.com>
2025-12-18 15:28:15 -0600

    I have working code for 3 part 2, but a flaw in my logic/approach...damn....

--- 2025/day3/lobby_part_2.rb
+++ 2025/day3/lobby_part_2.rb
@@ -18,6 +18,13 @@ def find_bottom_3(arr)
   bottom_3
 end
 
+def is_bigger_ahead?(num, posn, arr)
+  arr[posn..arr.length - 1].each do |n|
+    return true if n >= num
+  end
+  false
+end
+
 sum = 0
 
 File.open("input.txt").each_line do |line|
@@ -25,7 +32,21 @@ File.open("input.txt").each_line do |line|
   arr = line.chomp.chars.map(&:to_i)
 
   bottom_3 = find_bottom_3(arr)
+  puts "arr.length: #{arr.length}"
   puts "the bottom_3 of #{line} are #{bottom_3.to_a.to_s}"
+
+  delete_count = 0
+
+  arr.each_with_index do |num, posn|
+    if bottom_3.include?(num)
+      arr.delete_at(posn) if is_bigger_ahead?(num, posn, arr) and delete_count < 3
+      delete_count += 1
+      break if delete_count == 3
+    end
+  end
+
+  puts "arr.length: #{arr.length}"
+  puts "arr is now #{arr.to_s}"
 end
 
 puts "sum: #{sum}"
