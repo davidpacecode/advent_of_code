9edc272455352338ae3a54fd0f99f4be836e08c5
David Pace <david.pace@hey.com>
2025-12-22 15:20:31 -0600

    have day 5 part 1 done

--- 2025/day5/cafeteria.rb
+++ 2025/day5/cafeteria.rb
@@ -1,18 +1,25 @@
 #!/home/dsp2/.local/share/mise/installs/ruby/3.3.4/bin/ruby
 
-fresh_ranges = {}
+def is_fresh?(num,fresh_ranges)
+
+  fresh_ranges.each do |range|
+    if(num >= range.split('-')[0].to_i && num <= range.split('-')[1].to_i)
+      return true
+    end
+  end
+  return false
+end
+
+fresh_ranges = []
 fresh_count = 0
 
 File.foreach("input.txt") do |line|
-  puts line
   if line.include?('-')
-    (line.split('-')[0].to_i..line.split('-')[1].to_i).each do |i|
-      puts "i: #{i}"
-      fresh_ranges[i] = 1
-    end
+    fresh_ranges << line
   else
-    fresh_count += 1 if fresh_ranges[line.to_i] == 1
+    fresh_count += 1 if is_fresh?(line.to_i,fresh_ranges)
   end
 end
 
 puts "fresh_count: #{fresh_count}"
+
