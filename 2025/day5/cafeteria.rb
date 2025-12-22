cac6c8394b73b8cc8fa2baa688204f055796d916
davidpacecode <davidpace@gmail.com>
2025-12-22 14:11:35 -0600

    have a version of d5 part 1 but it's slow as s and need to test

--- 2025/day5/cafeteria.rb
+++ 2025/day5/cafeteria.rb
@@ -1,20 +1,18 @@
 #!/home/dsp2/.local/share/mise/installs/ruby/3.3.4/bin/ruby
 
-fresh_ranges = []
+fresh_ranges = {}
 fresh_count = 0
 
 File.foreach("input.txt") do |line|
+  puts line
   if line.include?('-')
     (line.split('-')[0].to_i..line.split('-')[1].to_i).each do |i|
+      puts "i: #{i}"
       fresh_ranges[i] = 1
     end
   else
     fresh_count += 1 if fresh_ranges[line.to_i] == 1
   end
-end
-
-fresh_ranges.each_with_index do |range, i|
-  puts "range #{i}: #{range}"
 end
 
 puts "fresh_count: #{fresh_count}"
